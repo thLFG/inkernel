7974d2081491d570b8411edef8909961d7649be7
Lotte <157680249+thLFG@users.noreply.github.com>
2026-06-16 18:26:31 +0300

    Delete inkernel directory

--- inkernel/installer/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let kernel_path = "target/x86_64-unknown-none/debug/inkernel";
-    let mut bootloader = bootloader::Bootlink::new(std::path::Path::new(kernel_path));
-    bootloader
-        .build_iso(std::path::Path::new("target/inkernel.iso"))
-        .unwrap();
-    println!("Holy moly!!1! This shit is work!");
-}
